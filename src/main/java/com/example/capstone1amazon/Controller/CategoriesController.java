01cde5343adef9b3e82e289c567772606f9ebe51
W2AlharbiMe <w2.alharbi.me@gmail.com>
2023-08-10 18:19:44 +0300

    feat: add endpoint save category

--- src/main/java/com/example/capstone1amazon/Controller/CategoriesController.java
+++ src/main/java/com/example/capstone1amazon/Controller/CategoriesController.java
@@ -1,12 +1,17 @@
 package com.example.capstone1amazon.Controller;
 
+import com.example.capstone1amazon.ApiResponse.ApiErrorResponse;
+import com.example.capstone1amazon.ApiResponse.ApiResponseWithData;
+import com.example.capstone1amazon.ApiResponse.SimpleApiResponse;
 import com.example.capstone1amazon.Model.Category;
 import com.example.capstone1amazon.Service.CategoryService;
+import com.example.capstone1amazon.Service.ErrorsService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
@@ -16,10 +21,29 @@ import java.util.ArrayList;
 public class CategoriesController {
 
     private final CategoryService categoryService;
+    private final ErrorsService errorsService;
+
 
     @GetMapping("/get")
     public ResponseEntity<ArrayList<Category>> getCategories() {
         return ResponseEntity.ok(categoryService.getCategories());
     }
+
+
+    @PostMapping("/save")
+    public ResponseEntity<?> saveCategory(@RequestBody @Valid Category category, Errors errors) {
+        if(categoryService.isDuplicateId(category.getId())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body((new ApiErrorResponse("category", "the id must be unique.", "id", "unique")));
+        }
+
+        if(errors.hasErrors()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorsService.bulkAdd(errors).get());
+        }
+
+        categoryService.saveCategory(category);
+        return ResponseEntity.ok((new ApiResponseWithData<Category>("The category have been created.", category)));
+    }
+
+
 }
 
--- src/main/java/com/example/capstone1amazon/Service/CategoryService.java
+++ src/main/java/com/example/capstone1amazon/Service/CategoryService.java
@@ -4,12 +4,23 @@ import com.example.capstone1amazon.Model.Category;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 @Service
 public class CategoryService {
-    private ArrayList<Category> categories = new ArrayList<>();
+    private final ArrayList<Category> categories = new ArrayList<>();
+    private final HashMap<Integer, Boolean> unique_index_map = new HashMap<>();
 
-    public ArrayList<Category> getCategories() {
+    public final ArrayList<Category> getCategories() {
         return categories;
+    }
+
+    public boolean isDuplicateId(Integer id) {
+        return unique_index_map.containsKey(id);
+    }
+
+    public void saveCategory(Category category) {
+        unique_index_map.put(category.getId(), true);
+        categories.add(category);
     }
 }
