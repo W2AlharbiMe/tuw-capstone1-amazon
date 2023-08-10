c9a9f08f6662e5a7067ef244f9338f50f98a7855
W2AlharbiMe <w2.alharbi.me@gmail.com>
2023-08-10 20:15:07 +0300

    feat: add product layers service, controller, model.

---
+++ src/main/java/com/example/capstone1amazon/Controller/ProductsController.java
@@ -0,0 +1,10 @@
+package com.example.capstone1amazon.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/products")
+public class ProductsController {
+
+}
