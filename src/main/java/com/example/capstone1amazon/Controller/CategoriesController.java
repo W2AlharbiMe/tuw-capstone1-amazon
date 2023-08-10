de6427623afbec82521cfd6f7145c757d697f067
W2AlharbiMe <w2.alharbi.me@gmail.com>
2023-08-10 14:38:47 +0300

    feat: add endpoint get categories

--- src/main/java/com/example/capstone1amazon/Controller/CategoriesController.java
+++ src/main/java/com/example/capstone1amazon/Controller/CategoriesController.java
@@ -1,11 +1,25 @@
 package com.example.capstone1amazon.Controller;
 
+import com.example.capstone1amazon.Model.Category;
+import com.example.capstone1amazon.Service.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+
 @RestController
 @RequestMapping("/api/v1/categories")
+@RequiredArgsConstructor
 public class CategoriesController {
 
+    private final CategoryService categoryService;
+
+    @GetMapping("/get")
+    public ResponseEntity<ArrayList<Category>> getCategories() {
+        return ResponseEntity.ok(categoryService.getCategories());
+    }
 }
 
--- src/main/java/com/example/capstone1amazon/Service/CategoryService.java
+++ src/main/java/com/example/capstone1amazon/Service/CategoryService.java
@@ -1,4 +1,15 @@
 package com.example.capstone1amazon.Service;
 
+import com.example.capstone1amazon.Model.Category;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
 public class CategoryService {
+    private ArrayList<Category> categories = new ArrayList<>();
+
+    public ArrayList<Category> getCategories() {
+        return categories;
+    }
 }
