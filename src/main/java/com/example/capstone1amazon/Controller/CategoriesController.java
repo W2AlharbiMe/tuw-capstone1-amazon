989fe5c5ccee9663ca5c5f89c389b921fcf4cf0d
W2AlharbiMe <w2.alharbi.me@gmail.com>
2023-08-10 14:20:06 +0300

    feat: add category layers service, controller, model.

---
+++ src/main/java/com/example/capstone1amazon/Controller/CategoriesController.java
@@ -0,0 +1,11 @@
+package com.example.capstone1amazon.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/categories")
+public class CategoriesController {
+
+}
+
---
+++ src/main/java/com/example/capstone1amazon/Service/CategoryService.java
@@ -0,0 +1,4 @@
+package com.example.capstone1amazon.Service;
+
+public class CategoryService {
+}
