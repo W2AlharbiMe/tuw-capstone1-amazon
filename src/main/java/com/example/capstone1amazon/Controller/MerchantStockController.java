73255321b6085c1c6bff544589cc7a23ad856ecf
W2AlharbiMe <w2.alharbi.me@gmail.com>
2023-08-11 20:46:07 +0300

    feat: add endpoint get merchants stocks

--- src/main/java/com/example/capstone1amazon/Controller/MerchantStockController.java
+++ src/main/java/com/example/capstone1amazon/Controller/MerchantStockController.java
@@ -1,12 +1,25 @@
 package com.example.capstone1amazon.Controller;
 
+import com.example.capstone1amazon.Model.MerchantStock;
+import com.example.capstone1amazon.Service.MerchantStockService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
 
 @RestController
 @RequestMapping("/api/v1/merchants/stock")
 @RequiredArgsConstructor
 public class MerchantStockController {
+
+    private final MerchantStockService merchantStockService;
+
+    @GetMapping("/get")
+    public ResponseEntity<Collection<MerchantStock>> getAllMerchantsStocks() {
+        return ResponseEntity.ok(merchantStockService.getAllMerchantsStocks());
+    }
 
 }
--- src/main/java/com/example/capstone1amazon/Service/MerchantStockService.java
+++ src/main/java/com/example/capstone1amazon/Service/MerchantStockService.java
@@ -1,8 +1,17 @@
 package com.example.capstone1amazon.Service;
 
+import com.example.capstone1amazon.Model.MerchantStock;
 import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.HashMap;
 
 @Service
 public class MerchantStockService {
 
+    private final HashMap<Integer, MerchantStock> merchantsStocks = new HashMap<>();
+
+    public Collection<MerchantStock> getAllMerchantsStocks() {
+        return merchantsStocks.values();
+    }
 }
