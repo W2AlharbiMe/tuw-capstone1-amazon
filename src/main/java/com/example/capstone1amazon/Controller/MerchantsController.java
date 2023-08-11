bbf40ed68a601cbb6d9cfad01cbbb8515d7542b4
W2AlharbiMe <w2.alharbi.me@gmail.com>
2023-08-11 20:05:19 +0300

    feat: add endpoint get merchants

--- src/main/java/com/example/capstone1amazon/Controller/MerchantsController.java
+++ src/main/java/com/example/capstone1amazon/Controller/MerchantsController.java
@@ -1,12 +1,26 @@
 package com.example.capstone1amazon.Controller;
 
+import com.example.capstone1amazon.Model.Merchant;
+import com.example.capstone1amazon.Service.MerchantService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
 
 @RestController
 @RequestMapping("/api/v1/merchants")
 @RequiredArgsConstructor
 public class MerchantsController {
+
+    private final MerchantService merchantService;
+
+    @GetMapping("/get")
+    public ResponseEntity<Collection<Merchant>> getAllMerchants() {
+        return ResponseEntity.ok(merchantService.getAllMerchants());
+    }
+
 
 }
--- src/main/java/com/example/capstone1amazon/Service/MerchantService.java
+++ src/main/java/com/example/capstone1amazon/Service/MerchantService.java
@@ -1,8 +1,23 @@
 package com.example.capstone1amazon.Service;
 
+import com.example.capstone1amazon.Model.Merchant;
 import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.HashMap;
 
 @Service
 public class MerchantService {
+
+    private final HashMap<Integer, Merchant> merchants = new HashMap<>();
+
+
+    public Collection<Merchant> getAllMerchants() {
+        return merchants.values();
+    }
+
+    public boolean containsId(Integer id) {
+        return merchants.containsKey(id);
+    }
 
 }
