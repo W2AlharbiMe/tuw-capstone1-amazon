e9b015f8fd394ce2d203a92c794f2d76dc4065d5
W2AlharbiMe <w2.alharbi.me@gmail.com>
2023-08-11 21:32:43 +0300

    feat: add endpoint create merchant stock

--- src/main/java/com/example/capstone1amazon/Controller/MerchantStockController.java
+++ src/main/java/com/example/capstone1amazon/Controller/MerchantStockController.java
@@ -1,12 +1,18 @@
 package com.example.capstone1amazon.Controller;
 
+import com.example.capstone1amazon.ApiResponse.ApiErrorResponse;
+import com.example.capstone1amazon.ApiResponse.ApiResponseWithData;
 import com.example.capstone1amazon.Model.MerchantStock;
+import com.example.capstone1amazon.Service.ErrorsService;
+import com.example.capstone1amazon.Service.MerchantService;
 import com.example.capstone1amazon.Service.MerchantStockService;
+import com.example.capstone1amazon.Service.ProductService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 
@@ -16,10 +22,42 @@ import java.util.Collection;
 public class MerchantStockController {
 
     private final MerchantStockService merchantStockService;
+    private final ProductService productService;
+    private final MerchantService merchantService;
+    private final ErrorsService errorsService;
+
 
     @GetMapping("/get")
     public ResponseEntity<Collection<MerchantStock>> getAllMerchantsStocks() {
         return ResponseEntity.ok(merchantStockService.getAllMerchantsStocks());
     }
+
+    @PostMapping("/create")
+    public ResponseEntity<?> createMerchantStock(@RequestBody @Valid MerchantStock merchantStock, Errors errors) {
+        if(merchantStockService.containsId(merchantStock.getId())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body((new ApiErrorResponse("merchantStock", "the id must be unique.", "id", "unique")));
+        }
+
+        if(!productService.containsId(merchantStock.getProductId())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body((new ApiErrorResponse("merchantStock", "No product have been found with the product id you provided.", "productId", "product_not_found")));
+        }
+
+        if(!merchantService.containsId(merchantStock.getMerchantId())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body((new ApiErrorResponse("merchantStock", "No merchant have been found with the merchant id you provided.", "merchantId", "merchant_not_found")));
+        }
+
+        if(merchantStockService.ensureOneProduct(merchantStock.getProductId(), merchantStock.getProductId())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body((new ApiErrorResponse("merchantStock", "merchant stock can not have the same product twice.", "productId", "unique_product")));
+        }
+
+        if(errors.hasErrors()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body((errorsService.bulkAdd(errors).get()));
+        }
+
+        merchantStockService.createMerchantStock(merchantStock);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body((new ApiResponseWithData<MerchantStock>("a merchant stock have been created.", merchantStock)));
+    }
+
 
 }
--- src/main/java/com/example/capstone1amazon/Service/MerchantStockService.java
+++ src/main/java/com/example/capstone1amazon/Service/MerchantStockService.java
@@ -3,6 +3,7 @@ package com.example.capstone1amazon.Service;
 import com.example.capstone1amazon.Model.MerchantStock;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 
@@ -10,8 +11,40 @@ import java.util.HashMap;
 public class MerchantStockService {
 
     private final HashMap<Integer, MerchantStock> merchantsStocks = new HashMap<>();
+    private final HashMap<Integer, ArrayList<Integer>> merchantProducts = new HashMap<>();
 
     public Collection<MerchantStock> getAllMerchantsStocks() {
         return merchantsStocks.values();
+    }
+
+    public boolean containsId(Integer id) {
+        return merchantsStocks.containsKey(id);
+    }
+
+    public void createMerchantStock(MerchantStock merchantStock) {
+        ArrayList<Integer> productsIds;
+
+        if(merchantProducts.containsKey(merchantStock.getMerchantId())) {
+            productsIds = merchantProducts.get(merchantStock.getMerchantId());
+            productsIds.add(merchantStock.getProductId());
+        } else {
+            productsIds = new ArrayList<>();
+            productsIds.add(merchantStock.getProductId());
+
+            merchantProducts.put(merchantStock.getMerchantId(), productsIds);
+        }
+
+        merchantsStocks.put(merchantStock.getId(), merchantStock);
+    }
+
+    // a merchant stock can not have the same product twice.
+    // each stock must have unique product id
+    public boolean ensureOneProduct(Integer productId, Integer merchantId) {
+        if(!merchantProducts.containsKey(merchantId)) {
+            return false;
+        }
+
+
+        return merchantProducts.get(merchantId).contains(productId);
     }
 }
