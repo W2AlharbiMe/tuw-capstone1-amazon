e6feb715897cd8e2b0e767841ef2f5982fc9aa53
W2AlharbiMe <w2.alharbi.me@gmail.com>
2023-08-11 18:50:59 +0300

    feat: add endpoint get products

--- src/main/java/com/example/capstone1amazon/Controller/ProductsController.java
+++ src/main/java/com/example/capstone1amazon/Controller/ProductsController.java
@@ -1,10 +1,25 @@
 package com.example.capstone1amazon.Controller;
 
+import com.example.capstone1amazon.Model.Product;
+import com.example.capstone1amazon.Service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collection;
+
 @RestController
 @RequestMapping("/api/v1/products")
+@RequiredArgsConstructor
 public class ProductsController {
 
+    private final ProductService productService;
+
+    @GetMapping("/get")
+    public ResponseEntity<Collection<Product>> getProducts() {
+        return ResponseEntity.ok(productService.getAllProducts());
+    }
 }
+
--- src/main/java/com/example/capstone1amazon/Service/ProductService.java
+++ src/main/java/com/example/capstone1amazon/Service/ProductService.java
@@ -1,8 +1,22 @@
 package com.example.capstone1amazon.Service;
 
+import com.example.capstone1amazon.Model.Product;
 import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.HashMap;
 
 @Service
 public class ProductService {
+    private final HashMap<Integer, Product> products = new HashMap<>();
 
+
+    public Collection<Product> getAllProducts() {
+        return products.values();
+    }
+
+
+    public boolean containsId(Integer id) {
+        return products.containsKey(id);
+    }
 }
