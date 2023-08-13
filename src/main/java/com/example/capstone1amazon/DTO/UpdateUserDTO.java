3e420b8ded04ebc4b5fa29e1f671249ea2ee98d1
W2AlharbiMe <w2.alharbi.me@gmail.com>
2023-08-13 05:21:01 +0300

    refactor: add password, role validation

--- src/main/java/com/example/capstone1amazon/DTO/UpdateUserDTO.java
+++ src/main/java/com/example/capstone1amazon/DTO/UpdateUserDTO.java
@@ -15,6 +15,7 @@ public class UpdateUserDTO {
 
     @NotEmpty(message = "the password field is required.")
     @Size(min = 7, message = "the password must be more than 6 length long.")
+    @Pattern(message = "the password must contain at least seven characters, at least one number and both lower and uppercase letters and special characters", regexp = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$")
     private String password;
 
 
@@ -23,7 +24,7 @@ public class UpdateUserDTO {
     private String email;
 
     @NotEmpty(message = "the role field is required.")
-//    @Pattern(message = "the role can only be 'Admin' or 'Customer'.", regexp = "")
+    @Pattern(message = "the position can only be either a 'admin' or 'customer'.", regexp = "(?i)\\b(admin)\\b?|(?i)\\b(customer)\\b")
     private String role;
 
     @NotNull(message = "the balance field is required.")
